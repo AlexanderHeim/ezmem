0d5978c789fb1adfabe40d5f9b229ea36f4fb20c
alexanderheim <alexanderheimat@gmail.com>
2020-08-08 02:18:58 +0200

    updated doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-//!ezmem provides basic functions for memory hacking.
-///Functions to read from memory.
+//! ezmem provides basic functions for memory hacking.
+/// Functions to read from memory.
 pub mod readmem;
-///Functions to gather information about processes.
+/// Functions to gather information about processes.
 pub mod process;
-///Functions to write to memory.
+/// Functions to write to memory.
 pub mod writemem;
 
 
